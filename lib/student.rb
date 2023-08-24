5e5e967f863c0682807ad05a465614277bbe868e
brunofay <brunofay1@hotmail.com>
2023-08-23 22:34:54 -0300

    NEW more stub exercicies

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,7 @@ class Student
   def has_finished?(course)
     # stub this
   end
+  def gender(sex)
+    #stub this
+  end
 end
--- spec/doubles/stub_spec.rb
+++ spec/doubles/stub_spec.rb
@@ -20,4 +20,35 @@ describe 'Stub test' do
     expect(course.has_completed?).to be_falsey
   end
 
-end
+  it 'dynamic params' do
+    student = Student.new
+    allow(student).to receive(:gender) do |arg|
+      if arg == 'male'
+        'he/his'
+      elsif arg == 'female'
+        'she/her'
+      else
+        'fluid'
+      end
+    end
+    expect(student.gender('female')).to eql('she/her')
+    expect(student.gender('male')).to eql('he/his')
+    expect(student.gender('liniker')).to eql('fluid')
+  end
+
+  it 'allow any instance of' do
+    bruno = Student.new
+    alessandra = Student.new
+    allow_any_instance_of(Student).to receive(:has_finished?)
+                                        .with('English')
+                                        .and_return(true)
+    expect(bruno.has_finished?('English')).to be_truthy
+    expect(alessandra.has_finished?('English')).to be_truthy
+  end
+
+  it('errors') do
+    student = Student.new
+    allow(student).to receive(:has_finished?).with('').and_raise("shit, that's a bug")
+    expect{student.has_finished?('')}.to raise_error("shit, that's a bug")
+    end
+  end
