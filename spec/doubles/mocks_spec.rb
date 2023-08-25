4de3be8fc8b685a00b6f163e21ca0389abc50a81
brunofay <brunofay1@hotmail.com>
2023-08-24 21:59:32 -0300

    NEW mock matchers to use

--- spec/doubles/mocks_spec.rb
+++ spec/doubles/mocks_spec.rb
@@ -11,4 +11,44 @@ describe 'Mocks' do
     #exercise
     student.gender('male')
   end
+  context 'call repeat method' do
+    it 'once/twice' do
+      student= Student.new
+      expect(student).to receive(:gender).with('male').once
+      student.gender('male')
+      expect(student).to receive(:gender).with('male').twice
+      student.gender('male')
+      student.gender('male')
+    end
+
+    it 'exactly' do
+      student= Student.new
+      expect(student).to receive(:gender).with('male').exactly(3).times
+      student.gender('male')
+      student.gender('male')
+      student.gender('male')
+    end
+
+    it 'at_least' do
+      student= Student.new
+      expect(student).to receive(:gender).with('male').at_least(:once)
+      student.gender('male')
+    end
+    it "at_least time" do
+      student= Student.new
+      expect(student).to receive(:gender).with('male').at_least(5).times
+      student.gender('male')
+      student.gender('male')
+      student.gender('male')
+      student.gender('male')
+      student.gender('male')
+      student.gender('male')
+    end
+  end
+
+  it 'mock return' do
+    student= Student.new
+    expect(student).to receive(:gender).and_return('555+558-*55-')
+    puts student.gender('male') # and_return stub return from method for any value
+  end
 end
